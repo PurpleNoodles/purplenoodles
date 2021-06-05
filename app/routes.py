4420d791083ba3594849f5d78e7154514478afa8
RoyalWeden <roy.weden@yahoo.com>
2021-06-04 23:52:15 -0500

    Create users and remove them

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,47 @@
 from flask.templating import render_template
-from app import app, config
-from flask import render_template, redirect, url_for, request
-from app.sqldb import sql_execute, create_account, create_user, create_report, print_table, drop_tables
+from app import app, config, sqldb
+from flask import render_template, redirect, url_for, request, session
 
 @app.route('/')
 def home():
-    sql_execute(create_account("Microsoft", "microsoft@microsoft.com", "mysecretpass"))
-    print_table("accounts")
-    sql_execute(drop_tables())
+    # Create Organization
+    sqldb.sql_execute(sqldb.create_account("Microsoft", "microsoft@microsoft.com", "securepass"))
+
+    # Create Users
+    sqldb.sql_execute(sqldb.create_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "John", "Smith", "johnsmith@microsoft.com", "smithpass", False))
+    sqldb.sql_execute(sqldb.create_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "Heather", "Wills", "heatherwills@microsoft.com", "willspass", False))
+    sqldb.sql_execute(sqldb.create_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "Steve", "West", "stevewest@microsoft.com", "steveypass", False))
+    sqldb.sql_execute(sqldb.create_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "George", "Jace", "georgejace@microsoft.com", "jacepass", False))
+    sqldb.sql_execute(sqldb.create_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "Mel", "Move", "melmove@microsoft.com", "melpass", False))
+    
+    # Create Report
+    sqldb.sql_execute(sqldb.create_report("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "9e29f498-2b9f-3c1e-8fd6-fb74a6fd26f8", 50, "This is the report."))
+
+    # Print Organizations
+    sqldb.print_table("accounts")
+    print()
+    # Print Users
+    sqldb.print_table("users")
+    print()
+    # Print Reports
+    sqldb.print_table("reports")
+    print()
+
+    # Remove User
+    sqldb.remove_user("ff7b23e8-0cc5-35fe-a472-8c1c277494ea", "microsoft@microsoft.com", "heatherwills@microsoft.com", "securepass")
+
+    print("\n\n-----------------------------------------------------------------------------------------------------------------------------\n")
+    # Print Organizations
+    sqldb.print_table("accounts")
+    print()
+    # Print Users
+    sqldb.print_table("users")
+    print()
+    # Print Reports
+    sqldb.print_table("reports")
+    print()
+
+    # Drop tables
+    sqldb.sql_execute(sqldb.drop_tables())
     return config
 
--- app/sqldb.py
+++ app/sqldb.py
@@ -1,5 +1,6 @@
 import psycopg2
 import uuid
+import hashlib
 
 conn = psycopg2.connect('')
 
@@ -7,19 +8,21 @@ def sql_execute(sql_command: str):
     with conn.cursor() as cur:
         cur.execute(sql_command)
 
+
 def create_account(name: str, email: str, password: str):
-    id = uuid.uuid3(uuid.NAMESPACE_X500, name)
+    id = uuid.uuid3(uuid.NAMESPACE_URL, name)
     return """
     CREATE TABLE IF NOT EXISTS accounts (
         id char(36) NOT NULL UNIQUE,
         name varchar NOT NULL UNIQUE,
         email varchar NOT NULL UNIQUE,
-        password text NOT NULL
+        password varchar NOT NULL
     );
     INSERT INTO accounts (id, name, email, password)
     VALUES ('{0}', '{1}', '{2}', '{3}')
     ON CONFLICT DO NOTHING;
-    """.format(id, name, email, password)
+    """.format(id, name, email, encrypt_pass(password)) + create_user(str(id), "admin", "admin", email, password, True)
+
 
 def create_user(organization_id: str, firstname: str, lastname: str, email: str, password: str, type: bool):
     id = uuid.uuid3(uuid.NAMESPACE_URL, organization_id + email)
@@ -29,18 +32,19 @@ def create_user(organization_id: str, firstname: str, lastname: str, email: str,
         organization_id char(36) NOT NULL,
         firstname varchar NOT NULL,
         lastname varchar NOT NULL,
-        email varchar NOT NULL,
-        password text NOT NULL,
+        email varchar NOT NULL UNIQUE,
+        password varchar NOT NULL,
         type boolean NOT NULL
     );
     INSERT INTO users (id, organization_id, firstname, lastname, email, password, type)
     VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', {6})
     ON CONFLICT DO NOTHING;
-    """.format(id, organization_id, firstname, lastname, email, password, str(type).lower())
+    """.format(id, organization_id, firstname, lastname, email, encrypt_pass(password), str(type).lower())
+
 
 def create_report(organization_id: str, user_id: str, severity: int, report: str):
-    id = uuid.uuid3(uuid.NAMESPACE_X500, organization_id + user_id + report)
-    return """"
+    id = uuid.uuid3(uuid.NAMESPACE_URL, organization_id + user_id + report)
+    return """
     CREATE TABLE IF NOT EXISTS reports (
         id char(36) NOT NULL UNIQUE,
         organization_id char(36) NOT NULL,
@@ -52,6 +56,27 @@ def create_report(organization_id: str, user_id: str, severity: int, report: str
     VALUES ('{0}', '{1}', '{2}', {3}, '{4}')
     ON CONFLICT DO NOTHING;
     """.format(id, organization_id, user_id, severity, report)
+
+
+def get_user(user_email, user_password):
+    with conn.cursor() as cur:
+        cur.execute("""
+        SELECT * FROM users
+        WHERE (email = '{0}' AND password = '{1}')
+        """.format(user_email, encrypt_pass(user_password)))
+        return cur.fetchone()
+
+
+def remove_user(organization_id, organization_email, user_email, organization_password): # returns boolean if deletion was successful
+    with conn.cursor() as cur:
+        if get_user(organization_id, organization_email, organization_password) != None:
+            cur.execute("""
+            DELETE FROM users
+            WHERE (organization_id = '{0}' AND email = '{1}')
+            """.format(organization_id, user_email))
+            return True
+        return False
+
 
 def print_table(table_name: str):
     print("---------- {0} ----------".format(table_name))
@@ -68,3 +93,9 @@ def drop_tables():
     DROP TABLE IF EXISTS users;
     DROP TABLE IF EXISTS reports;
     """
+
+def encrypt_pass(password: str):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# def check_pass(saved_password: str, password: str):
+#     return saved_password == hashlib.sha256(password.encode()).hexdigest()
